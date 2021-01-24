614f54a2e6952f0b905962b37f19246e6a72a381
Roman Turner <rn.turner@live.com>
2021-01-24 00:00:30 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,15 +1,45 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < coupons.length 
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
+    coupon_item_name = "#{coupons[counter][:item]} W/COUPON"
+    cart_item_with_coupon = find_item_by_name_in_collection(coupon_item_name, cart)
+    if cart_item && cart_item[:count] >= coupons[counter][:num]
+      if cart_item_with_coupon
+        cart_item_with_coupon[:count] += coupons[counter][:num] 
+        cart_item[:count] -= coupons[counter][:num]
+      else
+        cart_item_with_coupon = {
+          item: coupon_item_name,
+          price: (coupons[counter][:cost] / coupons[counter][:num]),
+          count: coupons[counter][:num],
+          clearance: cart_item[:clearance]
+        }
+        cart << cart_item_with_coupon
+        cart_item[:count] -= coupons[counter][:num]
+      end 
+   end 
+   counter += 1 
+  end 
+  cart
+end 
+
+def clearance_calc(n)
+  percent = n.to_f * 0.2 
+  n -= percent
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  counter = 0 
+  while counter < cart.length 
+  item_price = cart[counter][:price]
+  if cart[counter][:clearance]
+    item_price = clearance_calc(item_price)
+    end 
+  end 
+  cart 
 end
 
 def checkout(cart, coupons)
