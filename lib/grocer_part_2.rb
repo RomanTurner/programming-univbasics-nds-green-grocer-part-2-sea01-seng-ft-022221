b2f2b0bb4be1931e958e49d2a501b2e914def7b1
Roman Turner <rn.turner@live.com>
2021-01-24 01:23:32 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -52,6 +52,7 @@ def checkout(cart, coupons)
   total = final_cart[counter][:price] * final_cart[counter][:count]
   counter += 1 
 end 
+binding.pry
   if total > 100
   total -= (total * 0.10)
 end
